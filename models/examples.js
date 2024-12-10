f6cfdabe31e80362f741e2c05ea5e8e43b70753c
Orsouene <144720774+Orsouene@users.noreply.github.com>
2024-12-10 12:23:28 +0100

    Initial commit

---
+++ models/examples.js
@@ -0,0 +1,14 @@
+const examples = [
+  { id: 1, title: "Lorem ipsum dolor sit amet" },
+  { id: 2, title: "Consectetur adipiscing elit" },
+  { id: 3, title: "Sed do eiusmod tempor incididunt" },
+  { id: 4, title: "Ut labore et dolore magna aliqua" },
+  { id: 5, title: "Quis nostrud exercitation ullamco" },
+  { id: 6, title: "Laboris nisi ut aliquip ex ea commodo" },
+  { id: 7, title: "Duis aute irure dolor in reprehenderit" },
+  { id: 8, title: "In voluptate velit esse cillum dolore" },
+  { id: 9, title: "Eu fugiat nulla pariatur excepteur sint" },
+  { id: 10, title: "Occaecat cupidatat non proident sunt" },
+];
+
+module.exports = examples;
